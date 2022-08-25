4607985169a4b98b3a309b7ebf27e4be7ebd701c
Jin Choo <jinchoo315@gmail.com>
2022-08-24 23:35:08 -0500

    add lesson 3

---
+++ 3_numbers.js
@@ -0,0 +1,11 @@
+function genNextAge(age) {
+  return Number.parseInt(age, 10) + 1
+}
+
+function getBoxWidth(value) {
+  return Number.parseInt(value)
+}
+
+function covertNumberToString(number) {
+  return number.toString()
+}
